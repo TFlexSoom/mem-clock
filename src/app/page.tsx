5eb9763427192772282f3fd9f402225acd429f77
Tristan Hilbert (aka TFlexSoom) <tchilbert@live.com>
2025-04-11 18:14:22 -0400

    fix sql and add dayjs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,22 @@
+import { neon } from "@neondatabase/serverless";
+import dayjs from "dayjs";
+
 export default function Home() {
+  async function getTimestamp() {
+    'use server';
+
+    const sql = neon(`${process.env.DATABASE_URL}`);
+    const [row] = await sql`SELECT created_at FROM mem_clock_record ORDER BY created_at DESC;`;
+    console.log(row);
+    return row['created_at'];
+  }
+
+  async function getDaysSince() {
+    const createdAtTimestamp = await getTimestamp();
+    return dayjs().diff(createdAtTimestamp, 'days');
+  }
+
   return (
-    <div> This is a clock </div>
+    <div> {getDaysSince()} Days since last 9/11 Reference in Lab </div>
   );
 }
